eb1c3b409722554e91ac17adc3dd987053e5c7e8
Tega Richard <otegarichard@proton.me>
2025-11-24 21:00:27 +0100

    day 11: admin dashboard with sidebar and chart with charts.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const menuBtn = document.getElementById('burger');
+const sideBar = document.getElementById('sidebar');
+const year = document.getElementById('yearly');
+const month = document.getElementById('monthly');
+const cards = document.getElementById('cards');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    sidebar.classList.toggle('active');
+});
+document.querySelector('.search img').addEventListener('click', () => {
+    document.querySelector('.search input').classList.toggle('open');
+});
 function showSideBar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -6,3 +18,23 @@ function hideSideBar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+function yearly(){
+    year.style.backgroundColor = "var(--accent-color)";
+    year.style.color = "white";
+    month.style.backgroundColor = "transparent";   
+    month.style.color =  "var(--text-color)";
+    cards.classList.toggle('animate');
+    document.getElementById('basic-price').innerHTML = "<strong>$109</strong>/year"; 
+    document.getElementById('pro-price').innerHTML = "<strong>$216</strong>/year";
+    document.getElementById('premium-price').innerHTML = "<strong>$432</strong>/year";
+};
+function monthly(){
+    month.style.backgroundColor = "var(--accent-color)";
+    month.style.color = "white";
+    year.style.backgroundColor = "transparent";   
+    year.style.color = "var(--text-color)";
+    cards.classList.toggle('animate');
+    document.getElementById('basic-price').innerHTML = "<strong>$10</strong>/year"; 
+    document.getElementById('pro-price').innerHTML = "<strong>$20</strong>/year";
+    document.getElementById('premium-price').innerHTML = "<strong>$40</strong>/year";
+};
