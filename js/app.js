a3c5c8aaec5c418b726d96b298a41cc95ea03096
Tega Richard <otegarichard@proton.me>
2025-11-27 15:23:26 +0100

    Day 13: designed review section with autoscroll and cta button.

--- js/app.js
+++ js/app.js
@@ -52,3 +52,15 @@ new Chart(chart, {
         }]
     }
 });
+
+/*const slides = document.querySelectorAll('.card');
+            let current = 0;
+            function showSlide(index) {
+                slides.forEach((slide, i) => {
+                    slide.classList.toggle('active', i === index);
+                });
+            }
+            setInterval(() => {
+                current = (current + 1) % slides.length;
+                showSlide(current);
+            }, 5000)*/
